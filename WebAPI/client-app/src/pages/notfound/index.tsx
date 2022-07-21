6cd55dfa257c0ced160c614dc63da19757fc2eac
Віталік <vitalik.baziuk228@gmail.com>
2022-07-21 20:45:10 +0300

    Update not found

--- WebAPI/client-app/src/pages/notfound/index.tsx
+++ WebAPI/client-app/src/pages/notfound/index.tsx
@@ -1,20 +1,18 @@
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
+
+import Header from '../../containers/DefaultLayout/Header';
 
 const NotFound = () => {
     return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                minHeight: "100vh",
+        <>
+            <Header />
+            <Container component="main" sx={{
+                maxWidth: { xl: "xl", lg: "lg", md: "md" },
+                marginTop: "153px",
                 textAlign: "center",
-            }}
-        >
-            <Container component="main" sx={{ maxWidth: { xl: "xl", lg: "lg", md: "md" } }}>
+            }}>
                 <Typography sx={{ fontSize: "150px", fontWeight: "600", lineHeight: "188px", marginBottom: "20px" }}>404</Typography>
                 <Typography variant="h1" sx={{ marginBottom: "20px" }}>Page not found</Typography>
                 <Typography variant="h4" sx={{ fontWeight: "500", marginBottom: "95px" }}>
@@ -22,9 +20,22 @@ const NotFound = () => {
                     <br />
                     Please go back to the main page.
                 </Typography>
-                <Button variant="contained" href="/">Page home</Button>
+                <Button
+                    variant="contained"
+                    href="/"
+                    sx={{
+                        fontWeight: "500",
+                        fontSize: "20px",
+                        lineHeight: "25px",
+                        borderRadius: "10px",
+                        px: "65px",
+                        py: "15px",
+                        textTransform: "capitalize",
+                    }}>
+                    Page home
+                </Button>
             </ Container>
-        </Box>
+        </>
     );
 }
 
